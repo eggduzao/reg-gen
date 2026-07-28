809ecfffcf695843ca991f04c5c2e8d4ebff2950
Eduardo Gade Gusmao <eggduzao@gmail.com>
2026-07-28 12:54:38 -0300

    chore: remove old code

--- __netip9/ItemNotFoundException.cs
+++
@@ -1,8 +0,0 @@
-namespace Utilities.Exceptions;
-
-/// <summary>
-///     Signs that sequence doesn't contain any items that one was looking for.
-/// </summary>
-public class ItemNotFoundException : Exception
-{
-}
